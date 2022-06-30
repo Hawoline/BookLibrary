e8fcf3d4c26e926258609d7c6aebcff8e3ba47cd
Hawoline <belikhawoline@gmail.com>
2022-07-01 02:55:00 +0700

    - replaced MainActivity to view package

--- app/src/main/java/com/example/firebasefirestorelibrary/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/firebasefirestorelibrary/presentation/view/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.firebasefirestorelibrary;
+package com.example.firebasefirestorelibrary.presentation.view;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import com.example.firebasefirestorelibrary.R;
 
 public class MainActivity extends AppCompatActivity {
 
