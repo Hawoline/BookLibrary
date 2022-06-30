f6b2803441b587b8c604ad1e1932bad407c306b9
Hawoline <belikhawoline@gmail.com>
2022-07-01 02:51:49 +0700

    - added firebase; - began adding layouts

--- app/src/main/java/com/example/firebasefirestorelibrary/MainActivity.java
+++ app/src/main/java/com/example/firebasefirestorelibrary/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.firebasefirestorelibrary;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
