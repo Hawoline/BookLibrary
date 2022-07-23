854cd7daefa2b6ad2eedec244129890327085f06
Hawoline <belikhawoline@gmail.com>
2022-07-24 00:57:09 +0700

    - added searchview;

--- app/src/main/java/com/example/firebasefirestorelibrary/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/firebasefirestorelibrary/presentation/view/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.firebasefirestorelibrary.presentation.view;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.firebasefirestorelibrary.R;
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -16,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container_main, MainFragment.class, null)
                     .commit();
         }
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull @NotNull Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
+        return true;
 
     }
 }
